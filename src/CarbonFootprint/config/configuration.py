4d711beff7157f1a8dc0bd9131fc72932b8e0696
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-05-13 11:16:19 +0530

    data validation added

--- src/CarbonFootprint/config/configuration.py
+++ src/CarbonFootprint/config/configuration.py
@@ -1,6 +1,7 @@
 from CarbonFootprint.constants import *
 from CarbonFootprint.utils.common import read_yaml, create_directories
-from CarbonFootprint.entity.config_entity import DataIngestionConfig
+from CarbonFootprint.entity.config_entity import (DataIngestionConfig, 
+                                                  DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -30,3 +31,20 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = {}
+        schema.update(self.schema["COLUMNS"])
+        schema.update(self.schema["TARGET_COLUMN"])
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
